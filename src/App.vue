e1be1404c791a8dab27dc7d16821091bb0559a55
[Enrique Viveros] <github.villain713@slmail.me>
2025-11-19 08:36:56 +0100

    creating repo

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CardTitle from './components/CardTitle.vue';
+</script>
+
+<template>
+  <div>
+
+  </div>
+  <CardTitle  cardtitle ="antworten"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardTitle.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue'
+    defineProps({
+        cardtitle: String,
+    })
+</script>
+
+<template>
+    <h2> {{  cardtitle }}</h2>   
+</template>
+
+<style scoped>
+    h2 {
+        font-size: 2em;
+    }
+</style>
+
