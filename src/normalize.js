59e4fe3dd2b7e8384de5fd304cb5d9b5e2ef72f2
[Enrique Viveros] <github.villain713@slmail.me>
2026-01-23 07:58:56 +0100

    creating a normalization file

---
+++ src/normalize.js
@@ -0,0 +1,9 @@
+//import data from './data/temp.json' with { type: 'json'};
+import fs from 'fs';
+const data = JSON.parse(fs.readFileSync('./src/data/temp.json', 'utf8'));
+console.log(data)
+
+
+console.log("Data type", typeof data);
+console.log("Is array?", Array.isArray(data));
+console.log("Content", data);
