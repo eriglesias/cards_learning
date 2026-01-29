923da24dc7da1f868ba7b704400248659772f39d
[Enrique Viveros] <github.villain713@slmail.me>
2026-01-29 07:57:57 +0100

    doing some js tests for normalization

--- src/normalize.js
+++ src/normalize.js
@@ -8,3 +8,19 @@ console.log("Data type", typeof data);
 console.log("Is array?", Array.isArray(data));
 console.log("Content", data);
 
+
+console.log(data.cards.verben.eintraege.find(v=> v.id === "verb-geben"))
+
+/* === TEST === */
+
+const map = new Map();
+map.set("a", 1);
+map.set("b", 2);
+map.set("c", 3);
+console.log(map.get("a"));
+map.set("a",97);
+console.log(map.get("a"));
+console.log(map.size)
+map.delete("b");
+console.log(map.size)
+
