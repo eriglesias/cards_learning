80d187f22a53ccd536c465265c266db40ed325f9
[Enrique Viveros] <github.villain713@slmail.me>
2026-02-15 09:36:44 +0100

    feat: adding navigation guards to the verbs case using beforeEnter to go back to select in case the case is not in the verbsByCase object

--- src/router.js
+++ src/router.js
@@ -1,12 +1,23 @@
 import {createRouter, createMemoryHistory } from 'vue-router'
 import HomeView from './views/home-view.vue'
 import LearningVerbs from './components/learning-card.vue'
+import Normalization from './services/normalize'
 import SelectLearning from './views/learn-by.vue'
+
+
 const routes = [
     
     { path: '/', component: HomeView},
     { path: '/select', component: SelectLearning},
-    { path: '/learn/verbs/:case', component: LearningVerbs}, // props: true? 
+    { path: '/learn/verbs/:case', 
+        component: LearningVerbs,
+        beforeEnter: (to) => {
+             const caseParam = to.params.case
+            if (!(caseParam in Normalization.verbsByCase)) {
+            return '/select'
+          }
+        },
+    }, 
    
 ]
 
